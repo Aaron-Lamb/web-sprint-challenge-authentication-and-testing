a5c76f8761d849441d9d044891a4d9dac914c530
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-11-06 16:59:47 -0800

    Restrict function working

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,34 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+function restrict(){
+  return async (req, res, next) => {
+    try{
+      const token = req.cookies.token;
+
+      if(!token){
+        return res.status(401).json({
+          message: "You shall not pass"
+        })
+      }
+
+      jwt.verify(token, process.env.TOKEN, (error, decoded) => {
+        if(error){
+          return res.status(401).json({
+            message: "You shall not pass"
+          })
+        }
+
+        req.token = decoded;
+
+        next()
+      })
+    } catch(error){
+      console.log(error);
+    }
+  };
+  
+}
+
+module.exports = {
+  restrict
+}

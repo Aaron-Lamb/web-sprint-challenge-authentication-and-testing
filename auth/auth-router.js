9eb79f8d2f7d49d5e461c37951644b7a6243a1d3
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-11-06 17:21:16 -0800

    Model and register endpoint ready

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
+const auth = require('./auth-model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {restrict} = require('./authenticate-middleware');
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  try{
+    const {username, password} = req.body;
+    const user = await auth.getUserBy({ username });
+
+    if(user){
+      return res.status(409).json({
+        message: "Username is taken"
+      })
+    }
+
+    const newUser = await auth.addUser({
+      username,
+      password: await bcrypt.hash(password, 13)
+    })
+
+    return res.status(200).json({
+      message: `Welcome ${username}`
+    })
+
+  } catch(error){
+    console.log(error)
+  }
 });
 
 router.post('/login', (req, res) => {
